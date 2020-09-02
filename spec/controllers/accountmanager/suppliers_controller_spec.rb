d9539a8b90197613bd027aa9af7a4d26de7fd9ec
hoangnttpc <dang.quang.hoang@sun-asterisk.com>
2020-09-02 21:54:18 +0700

    [Task#28668][RSpec] - Viết Rspec cho controller Supplier

---
+++ spec/controllers/accountmanager/suppliers_controller_spec.rb
@@ -0,0 +1,135 @@
+require "rails_helper"
+require "spec_helper"
+include RSpecSessionHelper
+
+RSpec.describe Accountmanager::SuppliersController, type: :controller do
+  let(:accountmanager) {FactoryBot.create :user, role: User.roles[:accountmanager]}
+  let!(:supplier_1) {FactoryBot.create :supplier, user_id: accountmanager.id}
+  let!(:supplier_2) {FactoryBot.create :supplier, user_id: accountmanager.id}
+  let!(:supplier_3) {FactoryBot.create :supplier, user_id: accountmanager.id}
+  let(:invalid_supplier) {
+    FactoryBot.build(:supplier,
+      name: "",
+    ).as_json
+  }
+
+  before(:each) do
+    login_as_accountmanager
+  end
+
+  describe "GET suppliers#index" do
+    it "should render a list of suppliers" do
+      get :index
+      expect(assigns(:suppliers).size).to eq Supplier.all.size
+    end
+
+    it "should render :index view" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe "POST suppliers#new" do
+    it "should render :new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST suppliers#create" do
+    context "with valid attributes" do
+      it "should create new supplier object" do
+        get :new
+        expect(assigns(:supplier)).to be_a_new(Supplier)
+      end
+
+      it "should create a new supplier" do
+        @new_supplier = FactoryBot.create(:supplier, user_id: accountmanager.id)
+        expect {
+          post :create, params: {supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal")}
+        }.to change(Supplier, :count).by(1)
+      end
+
+      it "should redirect to the new supplier" do
+        post :create, params: {supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal")}
+        expect(response).to redirect_to accountmanager_suppliers_url
+      end
+    end
+
+    context "with invalid attribute(s)" do
+      it "should not create a new supplier" do
+        expect {
+          post :create, params: { supplier: invalid_supplier }
+        }.to_not change(Supplier, :count)
+      end
+
+      it "should re-render :new view" do
+        post :create, params: { supplier: invalid_supplier }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    context "with valid id" do
+      it "should render view edit" do
+        get :edit, params: {id: supplier_1.id}
+        expect(response).to render_template :edit
+      end
+    end
+  end
+  
+  describe "PUT suppliers#update/:id" do
+    before :each do
+      @supplier_to_update = FactoryBot.create :supplier
+    end
+
+    context "with valid attributes" do
+      it "should return a supplier" do
+        put :update, params: {
+            id: @supplier_to_update.id,
+            supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal")
+        }
+        expect(assigns(:supplier)).to eq @supplier_to_update
+      end
+
+      it "should update supplier's attributes" do
+        patch :update, params: {
+          id: @supplier_to_update, 
+          supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal", name: "hoangdq", address: "Hung Yen")
+        }
+        @supplier_to_update.reload
+        expect(@supplier_to_update.name).to eq "hoangdq"
+        expect(@supplier_to_update.address).to eq "Hung Yen"
+        expect(response).to be_redirect
+      end
+
+      it "should redirect to accountmanager_suppliers_path" do
+        put :update, params: {
+          id: @supplier_to_update.id,
+          supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal")
+        }
+        expect(response).to redirect_to accountmanager_suppliers_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "should not change supplier's attributes" do
+        put :update, params: {
+            id: @supplier_to_update,
+            supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal", taxNum: "123456")
+        }
+        @supplier_to_update.reload
+        expect(@supplier_to_update.taxNum).to_not eq("123456")
+      end
+
+      it "should re-render accountmanager edit supplier page" do
+        put :update, params: {
+            id: @supplier_to_update,
+            supplier: FactoryBot.attributes_for(:supplier, sup_type: "personal", taxNum: "123456")
+        }
+        expect(response).to render_template :edit
+      end
+    end
+  end
+end
